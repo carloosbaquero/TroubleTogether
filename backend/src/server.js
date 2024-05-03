e3d47034945827a3fbda54c0e01b8164f9788e3e
carloosbaquero <carlos.baqueville@gmail.com>
2024-05-03 11:27:12 +0200

    hotfix history api

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,14 @@ import config from '../config.js'
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
-
+import historyApiFallback from 'connect-history-api-fallback'
 import userRoutes from './routes/userRoutes.js'
 import travelRoutes from './routes/travelRoutes.js'
 import requestRoutes from './routes/requestRoutes.js'
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors(['http://localhost:3000', 'https://backend-globetrotters.onrender.com']))
 
 mongoose.connect(config.MONGO_HOST)
 
@@ -18,6 +18,7 @@ console.log(`NODE_ENV=${config.NODE_ENV}`)
 app.use('/api/users', userRoutes)
 app.use('/api/travels', travelRoutes)
 app.use('/api/requests', requestRoutes)
+app.use(historyApiFallback())
 
 app.listen(config.PORT, config.HOST, function () {
   console.log(`App listening on http://${config.HOST}:${config.PORT}`)
